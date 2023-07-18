db0ee84263d3fa32f7586be7520568bec314f52e
TrinhChung <chungtrinh2k2@gmail.com>
2023-07-18 16:23:18 +0700

    Login and Logout

--- pitch/views.py
+++ pitch/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    """View function for home page of site."""
+    # Generate counts of some of the main objects
+
+    context = {"var": "hello"}
+
+    return render(request, "index.html", context=context)
