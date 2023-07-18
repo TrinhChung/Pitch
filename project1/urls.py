e121152e0d67c6b9818982d7148fa38ab916bec3
TrinhChung <chungtrinh2k2@gmail.com>
2023-07-18 16:23:18 +0700

    Login and Logout

--- project1/urls.py
+++ project1/urls.py
@@ -16,7 +16,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from project1 import settings
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("pitch/", include("pitch.urls")),
+    path("", RedirectView.as_view(url="pitch/")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", RedirectView.as_view(url="login/")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# accounts/ login/ [name='login']
+# accounts/ logout/ [name='logout']
+# accounts/ password_change/ [name='password_change']
+# accounts/ password_change/done/ [name='password_change_done']
+# accounts/ password_reset/ [name='password_reset']
+# accounts/ password_reset/done/ [name='password_reset_done']
+# accounts/ reset/<uidb64>/<token>/ [name='password_reset_confirm']
+# accounts/ reset/done/ [name='password_reset_complete']
